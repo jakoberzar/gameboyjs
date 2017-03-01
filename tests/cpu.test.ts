0b5c6fa52c50045446be09adddc4f73736628d5a
Jakob Erzar <jakob.erzar@gmail.com>
2017-03-01 20:15:06 +0100

    Changed target to es2015, added testing suite. Mocha + chai + sinon, tested with typescript inside ts-node.

---
+++ tests/cpu.test.ts
@@ -0,0 +1,19 @@
+import 'mocha';
+import * as chai from 'chai';
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+
+import { CPU } from './../src/cpu';
+
+const expect = chai.expect;
+chai.use(sinonChai);
+
+describe('CPU', () => {
+
+  it('should write a message to console when initialized', sinon.test(function() {
+    const clog = this.stub(console, "log");
+    const cpu = new CPU();
+    expect(clog).to.be.calledWith("The CPU has been initialized! Again!");
+  }));
+
+});
