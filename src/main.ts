f8b1eb6bfc6a09d66f5efc9c3a0b63048173c4c3
Jakob Erzar <jakob.erzar@gmail.com>
2017-12-17 21:55:34 +0100

    Added Vue UI integration. Currently only displaying registers.

--- src/main.ts
+++ src/main.ts
@@ -6,34 +6,37 @@ import { Rom, RomInstruction } from './rom';
 
 const gbROM = './test_roms/pokemon_red.gb';
 
-function main() {
+export let cpu: CPU = new CPU();
 
-    ByteFileReader.loadFile(gbROM).then((val) => {
-        let rom: Rom = val;
-        let c: CPU = new CPU();
-        c.setRom(val);
+export async function main() {
+    return new Promise<CPU>((resolve, reject) => {
+        ByteFileReader.loadFile(gbROM).then((val) => {
+            let rom: Rom = val;
+            cpu.setRom(val);
 
-        rom.makeInstructions().then(() => {
-            c.registers.pc = 0x151;
-            while (rom.instAt(c.registers.pc) == null) {
-                c.registers.pc++;
-            }
-            let startTime = performance.now();
-            let instructionN = 100000;
-            for (let j = 0; j < instructionN; j++) {
-                c.readNext();
-            }
-            let endTime = performance.now();
-            let msSpent = endTime - startTime;
-            console.log('Executed ' + instructionN +
-                ' in ' + (endTime - startTime) + ' => ' +
-                instructionN / msSpent / 1000 + ' mHz');
-            console.log('done');
-            c.debugging = true;
-            c.readNext();
-            console.log(c.executedLog);
+            rom.makeInstructions().then(() => {
+                cpu.registers.pc = 0x151;
+                while (rom.instAt(cpu.registers.pc) == null) {
+                    cpu.registers.pc++;
+                }
+                let startTime = performance.now();
+                let instructionN = 100000;
+                for (let j = 0; j < instructionN; j++) {
+                    cpu.readNext();
+                }
+                let endTime = performance.now();
+                let msSpent = endTime - startTime;
+                console.log('Executed ' + instructionN +
+                    ' in ' + (endTime - startTime) + ' => ' +
+                    instructionN / msSpent / 1000 + ' mHz');
+                console.log('done');
+                cpu.debugging = true;
+                cpu.readNext();
+                console.log(cpu.executedLog);
+                resolve(cpu);
+            });
         });
-    });
-}
 
-main();
+    });
+
+}
---
+++ src/ui/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="root">
+        <h1>GameboyTS Debugger</h1>
+        <div v-if="gameTitle">
+            <span>You're playing</span>
+            <b>{{ gameTitle }}</b>
+        </div>
+        <register-component></register-component>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { cpu } from "./../main";
+
+import RegisterComponent from "./components/Registers.vue";
+
+export default Vue.extend({
+    data () {
+        return {
+            gameTitle: cpu.rom.gameTitle,
+        }
+    },
+    components: {
+        RegisterComponent,
+    }
+});
+</script>
+
+<style>
+body {
+    background-color: #222;
+    color: white;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+#root {
+    width: 800px;
+    margin: 0 auto;
+}
+.greeting {
+    font-size: 20px;
+}
+</style>
